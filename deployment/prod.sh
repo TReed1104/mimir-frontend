8cc170af9847d72e65c68419bd812d85f00f7b92
Tom Reed <t.reed1104@gmail.com>
2020-11-26 10:57:35 +0000

    Updated deployment scripts

--- deployment/prod.sh
+++ deployment/prod.sh
@@ -2,11 +2,11 @@
 echo ---------------------------------------
 echo Deployment Start - Production
 echo ---------------------------------------
-echo Building and Deploying Vue App
+echo Building and Deploying Mimir Frontend
 echo ---------------------------------------
-docker-compose -p vue-app -f docker-compose.yml up -d --build --remove-orphans
+docker-compose -p mimir-frontend -f docker-compose.yml up -d --build --remove-orphans
 echo
 echo ---------------------------------------
 echo Conntainer Status:
 echo ---------------------------------------
-docker ps | grep 'vue-app'
+docker ps | grep 'mimir-frontend'
