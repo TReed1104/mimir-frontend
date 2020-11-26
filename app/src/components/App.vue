2443d060e55321d605c106352af6c85be8dbabb0
Tom Reed <t.reed1104@gmail.com>
2020-11-26 11:04:11 +0000

    Migrated the App core component

--- app/src/components/App.vue
+++ app/src/components/App.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="root">
         <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#/">Webpack Template</a>
+            <a class="navbar-brand" href="#/">Mimir</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <router-link tag="a" class="nav-item nav-link" :to="{name: 'Home'}">Home</router-link>
+                    <router-link tag="a" class="nav-item nav-link" :to="{name: 'Index'}">Home</router-link>
+                    <TimetableDropDown></TimetableDropDown>
+                    <ManagementDropDown></ManagementDropDown>
                 </div>
             </div>
         </nav>
@@ -20,7 +22,13 @@
 </template>
 
 <script>
+    import 'bootstrap';
+    import TimetableDropDown from './menus/TimetableDropDown.vue'
+    import ManagementDropDown from './menus/ManagementDropDown.vue'
     export default {
-
+        components: {
+            TimetableDropDown,
+            ManagementDropDown
+        }
     }
 </script>
