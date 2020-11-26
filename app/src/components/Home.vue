4972ff983e7e51f1357b5af3f945dc6ec2bbfe9a
Tom Reed <t.reed1104@gmail.com>
2020-11-26 11:02:29 +0000

    Migrated the Home component

---
+++ app/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1 class="page-title">Mimir, The Timetabler</h1>
+        <p>
+            Welcome to Mimir, the timetabling system for the School of Computer Science.
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
