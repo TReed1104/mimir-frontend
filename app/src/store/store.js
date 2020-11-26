dd8696fc8c5cc66cbc74023e7ab78f3c77dc54f3
Tom Reed <t.reed1104@gmail.com>
2020-11-26 11:02:50 +0000

    Migrated vuex store

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Timetable from '../classes/Timetable.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
+        mimirAddress: "http://127.0.0.1/mimir-api",
+        currentTimetable: new Timetable("")
     },
     getters: {
+        // Endpoints
+        mimirServerAddress: state => state.mimirAddress,
+        mimirEndpointTimetables: state => { return state.mimirAddress + "/timetables"},
+        mimirEndpointBookings: state => { return state.mimirAddress + "/bookings?timetable="},
     },
     mutations: {
     },
